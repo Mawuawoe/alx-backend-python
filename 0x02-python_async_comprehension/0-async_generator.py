8d7b944bc57db4c4bdace5d84577c808122d2857
Mawuawoe <desmonddzakago@gmail.com>
2024-10-15 16:55:43 +0000

    task_0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Write a coroutine called async_generator
+that takes no arguments
+The coroutine will loop 10 times, each time asynchronously wait 1 second,
+then yield a random number between 0 and 10.
+"""
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    an async generator that
+    yield every 1 second a random
+    float
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
