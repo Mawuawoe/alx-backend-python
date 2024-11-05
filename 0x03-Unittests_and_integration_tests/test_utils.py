50256203679d71a703809c1d80438394d25ce55e
Mawuawoe <desmonddzakago@gmail.com>
2024-11-05 21:03:58 +0000

    task_3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,9 +2,11 @@
 """
 parameterized test for unittest
 """
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 import unittest
 from parameterized import parameterized
+from unittest.mock import Mock, patch
+import requests
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -31,3 +33,24 @@ class TestAccessNestedMap(unittest.TestCase):
 
         # Check that the exception message matches the expected key
         self.assertEqual(str(context.exception), f"'{expected_message}'")
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+        ])
+    @patch("requests.get")
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """
+        mocking requests.get
+        """
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        # call the function
+        result = get_json(test_url)
+
+        # test
+        mock_get.assert_called_once_with(test_url)
+
+        self.assertEqual(result, test_payload)
